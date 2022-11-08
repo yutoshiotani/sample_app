7cf6a460149f56838f3a4f0a11436a3c1f7a3152
EC2 Default User <ec2-user@ip-172-31-14-103.ap-northeast-1.compute.internal>
2022-11-08 03:58:32 +0000

    [Finish] 2-7

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,46 @@
+class ListsController < ApplicationController
+  
+  def new
+    @list = List.new
+  end
+  
+   # 以下を追加
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. トップ画面へリダイレクト
+    redirect_to list_path(list.id)
+  end
+  
+  def index
+    @lists = List.all
+  end
+  
+  def show
+    @list = List.find(params[:id])
+  end
+  
+  def edit
+    @list = List.find(params[:id])
+  end
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+  
+  def destroy
+     list = List.find(params[:id])
+     list.destroy
+     redirect_to '/lists'
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  
+  get 'top' => 'homes#top'
+  resources :lists
+  
+end
+  
